63a650507777fff4cdec5717eb4bd004bff76c82
Ajo Alen Jeen <ajoalenjeen333@gmail.com>
2026-02-20 16:41:47 -0500

    Add files via upload

---
+++ create_view.sql
@@ -0,0 +1,34 @@
+CREATE OR ALTER VIEW global_fashion AS
+WITH customer_revenue AS (
+    SELECT
+        t.[Customer Id] as customer_id,
+        t.[Store ID] as store_id,
+        CAST(t.date AS DATE) AS order_date,
+        SUM(t.[Line Total]) AS total_net_revenue,
+        COUNT(t.[Invoice ID]) AS num_orders,
+        MAX(c.country) AS customer_country,
+        MAX(c.city) AS customer_city,
+        MAX(s.country) AS store_country,
+        MAX(s.city) AS store_city,
+        MAX(c.name) AS full_name,
+        MAX(c.[Date Of Birth]) AS dob
+    FROM transactions t
+    INNER JOIN customers c ON c.[Customer Id] = t.[Customer Id]
+    INNER JOIN store s ON s.[Store ID] = t.[Store ID]
+    GROUP BY t.[Customer Id], t.[Store ID], CAST(t.date AS DATE)
+)
+SELECT
+    customer_id,
+    order_date,
+    total_net_revenue,
+    num_orders,
+    country_country,
+    country_city,
+    store_country,
+    store_city,
+    DATEDIFF(YEAR, dob, GETDATE()) AS age, 
+    full_name,
+    MIN(order_date) OVER (PARTITION BY customer_id) AS first_purchase_date,
+    YEAR(MIN(order_date) OVER (PARTITION BY customer_id)) AS cohort_year
+FROM customer_revenue;
+
